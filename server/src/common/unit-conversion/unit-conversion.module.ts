f0a755db76dab8b47e7e8ec8e446b4d29614bb92
bytelucas <lucasnguyenanh11@gmail.com>
2026-03-22 19:44:37 +0700

    refactor: switch UnitConversionModule to multi:true provider pattern

--- server/src/common/unit-conversion/unit-conversion.module.ts
+++ server/src/common/unit-conversion/unit-conversion.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
+import { UNIT_CONVERSION_STRATEGIES } from '@common/unit-conversion/unit-conversion.token';
 import { DistanceConversionStrategy } from '@common/unit-conversion/strategies/distance.strategy';
 import { TemperatureConversionStrategy } from '@common/unit-conversion/strategies/temperature.strategy';
 import { UnitConversionService } from '@common/unit-conversion/unit-conversion.service';
 
 @Module({
   providers: [
-    DistanceConversionStrategy,
-    TemperatureConversionStrategy,
+    {
+      provide: UNIT_CONVERSION_STRATEGIES,
+      useClass: DistanceConversionStrategy,
+      multi: true,
+    } as any,
+    {
+      provide: UNIT_CONVERSION_STRATEGIES,
+      useClass: TemperatureConversionStrategy,
+      multi: true,
+    } as any,
     UnitConversionService,
   ],
   exports: [UnitConversionService],
